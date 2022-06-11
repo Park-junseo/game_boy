9b295ac2f4889e47a5562b4d4b7e703e8a14adf5
Park-junseo <psbsanno@gmail.com>
2022-06-12 02:31:17 +0900

    except ultra

--- gpio/ultrasonic.py
+++ gpio/ultrasonic.py
@@ -105,15 +105,23 @@ class Ultrasonic(threading.Thread):
         distance = 0.0
         pulse_start = 0.0
         pulse_end = 0.0
+        inturrupt = 0.0
         GPIO.output(self.TRIG_PIN, False)
         time.sleep(0.5)
         GPIO.output(self.TRIG_PIN, True)
         time.sleep(0.00001)
         GPIO.output(self.TRIG_PIN, False)
+        inturrupt = time.time()
         while GPIO.input(self.ECHO_PIN) == 0 :
             pulse_start = time.time()
+            if pulse_start - inturrupt > 0.02 :
+                return self.distance
+
+        inturrupt = time.time()
         while GPIO.input(self.ECHO_PIN) == 1 :
             pulse_end = time.time()
+            if pulse_end - inturrupt > 0.02 :
+                return self.distance
         pulse_duration = pulse_end - pulse_start
         distance = pulse_duration * 17000
         distance = round(distance, 2)
